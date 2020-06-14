434b44056c6d936f585de064a99c50d4cfdf0e98
facundob <facundob@nimble.la>
2020-06-14 03:10:47 -0300

    ADD: color helper and beige color

---
+++ DonAr/Helpers/ColorHelper.swift
@@ -0,0 +1,17 @@
+//
+//  ColorHelper.swift
+//  DonAr
+//
+//  Created by macbook on 6/14/20.
+//  Copyright © 2020 Facundo Biolatto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorHelper: NSObject {
+    
+    class func beige() -> UIColor {
+        return UIColor(red: 251/255, green: 248/255, blue: 233/255, alpha: 1)
+    }
+}
---
+++ DonAr/SelectLoginTypeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  DonAr
+//
+//  Created by macbook on 5/24/20.
+//  Copyright © 2020 Facundo Biolatto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SelectLoginTypeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func signUpPressed() {
+        
+    }
+    
+    @IBAction func loginPressed() {
+        
+    }
+}
+
