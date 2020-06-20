f90a514d239b548d5123387449783cd7acc6b05d
facundob <facundob@nimble.la>
2020-06-20 18:15:51 -0300

    ADD: stlye maroon btn

--- DonAr/Extensions/UIButtonStyles.swift
+++ DonAr/Extensions/UIButtonStyles.swift
@@ -11,13 +11,12 @@ import UIKit
 
 extension UIButton {
     
-    func style() {
-        self.backgroundColor = ColorHelper.cyan()
+    func styleMaroonNoCorners() {
+        self.backgroundColor = ColorHelper.maroon()
         self.layer.borderWidth = 0
         self.layer.cornerRadius = 3
-        self.setTitle(self.titleLabel?.text?.uppercased(), for: UIControl.State())
         self.setTitleColor(UIColor.white, for: UIControl.State())
-        self.titleLabel?.font = UIFont.bambinoMediumFont(16)
+        //self.titleLabel?.font = UIFont.bambinoMediumFont(16)
         self.isUserInteractionEnabled = true
         self.alpha = 1
     }
